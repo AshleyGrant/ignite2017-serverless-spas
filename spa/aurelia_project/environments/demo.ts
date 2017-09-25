797c1deb5b9330c899cb647d34968244f08502a3
Ashley Grant <ashley@ashleygrant.com>
2017-09-25 14:19:35 -0400

    Try out server transpile

--- spa/aurelia_project/environments/demo.ts
+++ spa/aurelia_project/environments/demo.ts
@@ -1,6 +1,6 @@
 export default {
   debug: true,
   testing: true,
-  apiBaseUrl: '',
-  apiKey: ''
+  apiBaseUrl: 'https://serverless-built-spas.azurewebsites.net/api/',
+  apiKey: '3tT/8FyKh7X/kuP1pMokum08gOPBGobtv/jR1hDiTuOOa6kbzrxPvw=='
 };
